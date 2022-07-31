9fd9a2fd8c0f201fa094ccf80c9155002ae66b60
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-07-31 19:14:05 +0530

    yes me

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let bodyParser = require('body-parser');
-let cors = require('cors');
-let dotenv = require('dotenv');
+let bodyParser = require("body-parser");
+let cors = require("cors");
+let dotenv = require("dotenv");
 dotenv.config();
 let port = process.env.PORT || 9870;
-let mongo = require('mongodb');
+let mongo = require("mongodb");
 let MongoClient = mongo.MongoClient;
 //let mongoUrl = process.env.MongoUrl;
 let mongoUrl = process.env.MongoLiveUrl;
@@ -16,22 +16,38 @@ app.use(bodyParser.json());
 app.use(cors());
 // app.use(express.json());
 //1 for checking is server is on or not
-app.get('/', (req, res) => {
-    res.send("Express server is running");
+app.get("/", (req, res) => {
+  res.send("Express server is running");
 });
 //2 to check each collection
-app.get('/:collections', (req, res) => {
-    db.collection(req.params.collections).find().toArray((err, result) => {
-        if (err) throw err;
-        res.send(result)
+app.get("/Data", (req, res) => {
+  db.collection("Data")
+    .find()
+    .toArray((err, result) => {
+      if (err) throw err;
+      res.send(result);
     });
+});
+//9 to placeorder
+app.post("/Data/Add", (req, res) => {
+  db.collection("Data").insertMany(req.body, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+//3 to delete document from the collection
+app.delete("/Data/:id", (req, res) => {
+  db.collection("Data").deleteOne({ _id: req.params.id }, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 //Connection with db
 MongoClient.connect(mongoUrl, (err, client) => {
-    if (err) console.log(`Error While Connecting`);
-    db = client.db('User');
-    app.listen(port, (err) => {
-        if (err) throw err;
-        console.log(`Express Server listening on port ${port}`)
-    });
+  if (err) console.log(`Error While Connecting`);
+  db = client.db("User");
+  app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Express Server listening on port ${port}`);
+  });
 });
---
+++ myDo.md
@@ -0,0 +1,3 @@
+// heroku logs --tail -a appname
+// User data
+https://testapimdkaif.herokuapp.com/Data
